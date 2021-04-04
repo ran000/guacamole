e27ad652ab18c422360aae7998a9ad7176284987
Alexandre Roumiantsev <ran@mail.wplus.net>
2021-04-04 16:15:47 +0300

    working nginx

--- prepare.sh
+++
@@ -1,1 +0,0 @@
-openssl req -nodes -newkey rsa:2048 -new -x509 -keyout /opt/nginx/ssl/self-ssl.key -out /opt/nginx/ssl/self.cert -subj '/C=RU/ST=St.Petersburg/L=St.Petersburg/O=UNIX Education Center/OU=Training/CN=foundation0.ilt.example.com/emailAddress=instructor@ilt.example.com'
